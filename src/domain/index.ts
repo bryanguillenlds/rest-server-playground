122bb8404728bc56c0f5178f9f64c7f0db7d1759
Bryan Guillén <bryanguillenlds@gmail.com>
2025-11-07 14:52:25 -0400

    Refactor TodosController to use Use Cases for CRUD operations
    - Updated TodosController methods to utilize new Use Case implementations for CRUDing Todos.
    - Removed direct repository calls in favor of structured Use Case execution, enhancing separation of concerns and maintainability.
    - Added necessary imports for the new Use Cases in the controller and updated error handling for improved response management.

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,3 +2,9 @@ export * from "./datasources/todo.datasource";
 export * from "./repositories/todo.repository";
 export * from "./entities/todo.entity";
 export * from "./dtos";
+
+export * from "./use-cases/todos/create-todos";
+export * from "./use-cases/todos/update-todos";
+export * from "./use-cases/todos/get-todos";
+export * from "./use-cases/todos/get-todo";
+export * from "./use-cases/todos/delete-todos";
--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,47 +1,72 @@
 import { Request, Response } from "express";
-import { prisma } from "../../data/postgres";
 import { CreateTodoDto } from "../../domain/dtos";
 import { UpdateTodoDto } from "../../domain/dtos";
 import { TodoRepository } from "../../domain/repositories/todo.repository";
+import { GetTodosUseCaseImpl } from "../../domain/use-cases/todos/get-todos";
+import { GetTodoUseCaseImpl } from "../../domain/use-cases/todos/get-todo";
+import { CreateTodoUseCaseImpl } from "../../domain/use-cases/todos/create-todos";
+import { UpdateTodoUseCaseImpl } from "../../domain/use-cases/todos/update-todos";
+import { DeleteTodoUseCaseImpl } from "../../domain";
 
 export class TodosController {
   //* DI
   constructor(private readonly todoRepository: TodoRepository) {}
 
   //* GET /api/todos
-  public getTodos = async (req: Request, res: Response) => {
-    const todos = await this.todoRepository.getAll();
-    return res.json(todos);
+  public getTodos = (req: Request, res: Response) => {
+    new GetTodosUseCaseImpl(this.todoRepository)
+      .execute()
+      .then((todos) => {
+        return res.status(200).json(todos);
+      })
+      .catch((error) => {
+        if (error instanceof Error && error.message.includes("not found")) {
+          return res.status(404).json({ ERROR: error.message });
+        }
+
+        return res.status(500).json({ ERROR: "Internal server error" });
+      });
   };
 
-  public getTodoById = async (req: Request, res: Response) => {
+  public getTodoById = (req: Request, res: Response) => {
     const id = +req.params.id;
 
-    try {
-      const todo = await this.todoRepository.findById(id);
-      return res.json(todo);
-    } catch (error) {
-      if (error instanceof Error && error.message.includes("not found")) {
-        return res.status(404).json({ ERROR: error.message });
-      }
+    new GetTodoUseCaseImpl(this.todoRepository)
+      .execute(id)
+      .then((todo) => {
+        return res.status(200).json(todo);
+      })
+      .catch((error) => {
+        if (error instanceof Error && error.message.includes("not found")) {
+          return res.status(404).json({ ERROR: error.message });
+        }
 
-      return res.status(500).json({ ERROR: "Internal server error" });
-    }
+        return res.status(500).json({ ERROR: "Internal server error" });
+      });
   };
 
-  public createTodo = async (req: Request, res: Response) => {
+  public createTodo = (req: Request, res: Response) => {
     const [error, createTodoDto] = CreateTodoDto.create(req.body);
 
     if (error) {
       return res.status(400).json({ ERROR: error });
     }
 
-    const todo = await this.todoRepository.create(createTodoDto!);
+    new CreateTodoUseCaseImpl(this.todoRepository)
+      .execute(createTodoDto!)
+      .then((todo) => {
+        return res.status(201).json(todo);
+      })
+      .catch((error) => {
+        if (error instanceof Error && error.message.includes("not found")) {
+          return res.status(404).json({ ERROR: error.message });
+        }
 
-    return res.status(201).json(todo);
+        return res.status(500).json({ ERROR: "Internal server error" });
+      });
   };
 
-  public updateTodo = async (req: Request, res: Response) => {
+  public updateTodo = (req: Request, res: Response) => {
     const [error, updateTodoDto] = UpdateTodoDto.create({
       ...req.body,
       id: req.params.id,
@@ -51,40 +76,40 @@ export class TodosController {
       return res.status(400).json({ ERROR: error });
     }
 
-    try {
-      const updatedTodo = await this.todoRepository.updateById(
-        updateTodoDto!.id,
-        updateTodoDto!
-      );
+    new UpdateTodoUseCaseImpl(this.todoRepository)
+      .execute(updateTodoDto!)
+      .then((todo) => {
+        return res.status(200).json(todo);
+      })
+      .catch((error) => {
+        if (error instanceof Error && error.message.includes("not found")) {
+          return res.status(404).json({ ERROR: error.message });
+        }
 
-      return res.json(updatedTodo);
-    } catch (error) {
-      if (error instanceof Error && error.message.includes("not found")) {
-        return res.status(404).json({ ERROR: error.message });
-      }
-
-      return res.status(500).json({ ERROR: "Internal server error" });
-    }
+        return res.status(500).json({ ERROR: "Internal server error" });
+      });
   };
 
-  public deleteTodo = async (req: Request, res: Response) => {
+  public deleteTodo = (req: Request, res: Response) => {
     const id = +req.params.id;
 
     if (isNaN(id)) {
       return res.status(400).json({ ERROR: `ID is not a number` });
     }
 
-    try {
-      const deletedTodo = await this.todoRepository.deleteById(id);
+    new DeleteTodoUseCaseImpl(this.todoRepository)
+      .execute(id)
+      .then((todo) => {
+        return res
+          .status(200)
+          .json({ message: `Todo with ID: ${id} deleted`, todo });
+      })
+      .catch((error) => {
+        if (error instanceof Error && error.message.includes("not found")) {
+          return res.status(404).json({ ERROR: error.message });
+        }
 
-      return res
-        .status(200)
-        .json({ message: `Todo with ID: ${id} deleted`, deletedTodo });
-    } catch (error) {
-      if (error instanceof Error && error.message.includes("not found")) {
-        return res.status(404).json({ ERROR: error.message });
-      }
-      return res.status(500).json({ ERROR: "Internal server error" });
-    }
+        return res.status(500).json({ ERROR: "Internal server error" });
+      });
   };
 }
