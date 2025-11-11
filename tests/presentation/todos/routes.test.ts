8a6d5c8bfb33f1f20c2ff66a62d5973bd5502bd5
Bryan Guillén <bryanguillenlds@gmail.com>
2025-11-11 19:03:54 -0400

    Todo Update Validation and Route Testing
    - Added validation to ensure at least one field (title or completedAt) is provided when updating a Todo.
    - Expanded test suite for TodosRoutes to cover various scenarios including reading, creating, updating, and error handling for Todos.
    - Implemented cleanup in tests to ensure a fresh state before each test run.

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -1,18 +1,116 @@
 import request from "supertest";
 import { testServer } from "../../test-server";
+import { prisma } from "../../../src/data/postgres";
 
 describe("TodosRoutes.ts", () => {
   beforeAll(async () => {
     await testServer.start();
   });
 
+  beforeEach(async () => {
+    await prisma.todo.deleteMany();
+  });
+
   afterAll(() => {
     testServer.close();
   });
 
+  const todo1 = {
+    title: "Todo 1",
+  };
+
+  const todo2 = {
+    title: "Todo 2",
+  };
+
   it("should return TODOs api/todo", async () => {
+    await prisma.todo.createMany({
+      data: [todo1, todo2],
+    });
+
     const response = await request(testServer.app)
       .get("/api/todos")
       .expect(200);
+
+    expect(response.body).toHaveLength(2);
+    expect(response.body[0].title).toBe(todo1.title);
+    expect(response.body[1].title).toBe(todo2.title);
+  });
+
+  it("should return single TODO api/todo/:id", async () => {
+    const todo = await prisma.todo.create({
+      data: todo1,
+    });
+
+    const { body } = await request(testServer.app)
+      .get(`/api/todos/${todo.id}`)
+      .expect(200);
+
+    expect(body.id).toEqual(todo.id);
+  });
+
+  it("should return 404 if TODO not found api/todo/:id", async () => {
+    const { body } = await request(testServer.app)
+      .get(`/api/todos/1000`)
+      .expect(404);
+
+    expect(body.ERROR).toBe("Todo with ID: 1000 not found");
+  });
+
+  it("should create a new TODO api/todo", async () => {
+    const { body } = await request(testServer.app)
+      .post(`/api/todos`)
+      .send({ title: "New Todo" })
+      .expect(201);
+
+    expect(body.id).toBeDefined();
+    expect(body.title).toBe("New Todo");
+  });
+
+  it("should return 400 if title is not provided api/todo", async () => {
+    const { body } = await request(testServer.app)
+      .post(`/api/todos`)
+      .send({})
+      .expect(400);
+
+    expect(body.ERROR).toBe("Title is required");
+  });
+
+  it("should update a TODO api/todo/:id", async () => {
+    const todo = await prisma.todo.create({
+      data: todo1,
+    });
+
+    const { body } = await request(testServer.app)
+      .patch(`/api/todos/${todo.id}`)
+      .send({ title: "Updated Todo" })
+      .expect(200);
+
+    expect(body.id).toEqual(todo.id);
+    expect(body.title).toBe("Updated Todo");
+  });
+
+  it("should return 400 if no fields are provided api/todo/:id", async () => {
+    const todo = await prisma.todo.create({
+      data: todo1,
+    });
+
+    const { body } = await request(testServer.app)
+      .patch(`/api/todos/${todo.id}`)
+      .send({})
+      .expect(400);
+
+    expect(body.ERROR).toBe(
+      "At least one field (title or completedAt) must be provided"
+    );
+  });
+
+  it("should return 404 if TODO not found api/todo/:id", async () => {
+    const { body } = await request(testServer.app)
+      .patch(`/api/todos/1000`)
+      .send({ title: "Updated Todo" })
+      .expect(404);
+
+    expect(body.ERROR).toBe("Todo with ID: 1000 not found");
   });
 });
